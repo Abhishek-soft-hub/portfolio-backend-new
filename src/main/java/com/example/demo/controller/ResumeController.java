35f7569d3a95c2141a66cd59d63f329d2e09014f
Abhishek-soft-hub <236396774+Abhishek-soft-hub@users.noreply.github.com>
2026-05-26 16:08:12 +0530

    resume are added

---
+++ src/main/java/com/example/demo/controller/ResumeController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResumeController {
+
+	
+	@GetMapping("download-resume")
+	public ResponseEntity<Resource> downloadResume() {
+
+		Resource resource = new ClassPathResource("static/AbhishekResumeNew.pdf");
+
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF)
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=resume.pdf").body(resource);
+
+	}
+
+}
